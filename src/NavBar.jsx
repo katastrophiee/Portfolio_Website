3d4ad941d4a05ecfe7d9c439aee30ebd0075a217
kaytl <kaytl@DESKTOP-TI4LR7A>
2023-10-06 18:40:29 +0100

    slidy slide slides sliding

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,11 +1,14 @@
 import "./Styles/NavBarStyle.css";
 import { Link, useMatch, useResolvedPath } from 'react-router-dom';
+import Logo from "./Assets/name.png";
 
 export default function NavBar() {
      return (
         <nav className="navbar">
-            <Link to="/" className="home-page-link"><img src="./Assets/name.png" alt="Kaytlen" /></Link>
+            <img className="logo" src={Logo} alt="" />
+            {/* <Link to="/" className="home-page-link">Home</Link> */}
             <ul>
+                <CustomLink to="/" buttonText="Home" />
                 <CustomLink to="/Screens/About" buttonText="About" />
                 <CustomLink to="/Screens/Art" buttonText="Art" />
                 <CustomLink to="/Screens/Funny" buttonText="Funny" />
--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -1,10 +1,12 @@
 import '../Styles/HomePageStyle.css';
-import React from "react";
+
+import { Link, Route, Routes } from 'react-router-dom'
+import React, { useState } from "react";
+
 import NavBar from "../NavBar";
 import About from "./About";
 import Art from "./Art";
 import Funny from "./Funny";
-import { Route, Routes } from 'react-router-dom'
 
 //npm run dev -- start
 //ctrl + c on terminal -- stop
@@ -27,18 +29,46 @@ export default function App() {
 import { me } from "./About";
 
 function Home() {
+  // hooks can only be used in functions, not classes
+  var [slideOne, setSlideOne] = useState('slide');
+  var [slideTwo, setSlideTwo] = useState('slide hidden');
+
+  function ChangeToSlideTwo() {
+    setSlideOne(prev => prev = 'slide slide-left-onscreen')
+    setSlideTwo(prev => prev = 'slide slide-right-offscreen')
+  }
+
+  function ChangeToSlideOne() {
+    setSlideOne(prev => prev = 'slide slide-left-offscreen')
+    setSlideTwo(prev => prev = 'slide slide-right-onscreen')
+  }
+
   return (
-    <>
-      <div className='square'>
-        <h1 className='welcome-title'>Hello!</h1>
-          <div className='welcome-message'>
+    <div>
+      <div className={slideOne}>
+        <h1 className='home-welcome-title'>Hello!</h1>
+          <p className='home-welcome-message'>
             I'm not entirely sure what you put on portfolio websites tbh, but my name is {me.FirstName}.
-            I do software engineering unfortunately, as well as doing art sometimes. I enjoy all things that are stupid and silly.
-          </div>
+            I do software engineering unfortunately, as well as doing art sometimes. 
+            I also enjoy stupid things because I suffer from a tragic case of brain rot.
+          </p>
+          <button className='slide-change-button' onClick={ChangeToSlideTwo}>&gt;</button>
       </div>
-    </>
+      <div className={slideTwo}>
+        <p>
+          Check out some more interesting things here.
+        </p>
+        <ul>
+          <Link to='/'>About</Link>
+          <Link to='/'>Art</Link>
+          <Link to='/'>Funny</Link>
+        </ul>
+        <button className='slide-change-button' onClick={ChangeToSlideOne}>&lt;</button>
+      </div>
+    </div>
   )
 }
+
 
 function NotFound() {
   return (
